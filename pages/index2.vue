d4c32518f6a2cb62cf2fef0297af3a3eb37cccf5
Luiza Rafaela Padilha <luizarafaela1211@gmail.com>
2021-06-22 17:18:20 -0300

    novos ajustes

--- pages/index2.vue
+++ pages/index2.vue
@@ -1,48 +1,54 @@
 <template>
-  <div class="container">
-    <div class="row linha1">
-      <div class="col-sm-12 col-md-12 col-lg-6 coluna1">
-        <div class="conteudo-coluna1 col-12 px-0">
-          <div class="logo">
-            <img src="@/static/images/SimulaCred.svg" alt="Logo SimulaCred" />
+  <div class="fundo">
+    <div class="container">
+      <div class="row linha1">
+        <div class="col-sm-12 col-md-12 col-lg-6 coluna1">
+          <div class="conteudo-coluna1 col-12 px-0">
+            <div class="logo">
+              <img src="@/static/images/SimulaCred.svg" alt="Logo SimulaCred" />
+            </div>
+            <p class="subtitulo mt-4">Crédito rápido e fácil.</p>
+            <p class="legenda mt-4 col-8 px-0">
+              Com a nossa empresa, fazer um empréstimo é muito simples. Contamos
+              com métodos que facilitam esse processo.
+            </p>
+            <p class="chamada col-8 px-0">
+              Simule em nossa ferramenta as condições de crédito
+            </p>
           </div>
-          <p class="subtitulo mt-3">Crédito rápido e fácil.</p>
-          <p class="legenda mt-3 col-8 px-0">
-            Com a nossa empresa, fazer um empréstimo é muito simples. Contamos
-            com métodos que facilitam esse processo.
-          </p>
-          <p class="chamada col-8 px-0">
-            Simule em nossa ferramenta as condições de crédito
-          </p>
         </div>
-      </div>
-      <div class="col-sm-12 col-md-12 col-lg-6 coluna2">
-        <div class="conteudo-coluna2 col-12 px-4 py-2">
-          <h2>Consulte as condições</h2>
-          <div class="formulario">
-            <p class="tituloForm">Consulte as condições</p>
-            <section class="digiteValor">
+        <div class="col-sm-12 col-md-12 col-lg-6 coluna2">
+          <div class="conteudo-coluna2 col-12 px-4 py-2">
+            <h2>Consulte as condições</h2>
+            <div class="formulario">
               <p class="labelValor">Digite um valor para simular:</p>
-              <span> R$ </span>
-              <input id="valor" placeholder="1000,00" type="number" />
-            </section>
-            <b-form-group id="input-group" label="Nome:" label-for="input-2">
-              <b-form-input id="input" required></b-form-input>
-            </b-form-group>
-            <b-form-group id="input-group" label="Email:" label-for="input-2">
-              <b-form-input id="input2" required></b-form-input>
-            </b-form-group>
-            <b-form-group
-              id="input-group"
-              label="Telefone:"
-              label-for="input-2"
-            >
-              <b-form-input id="input3" required></b-form-input>
-            </b-form-group>
-            <b-form-checkbox id="checkbox-1" name="checkbox-1">
-              Sim, aceito receber emails, mensagens e ligações da empresa.
-            </b-form-checkbox>
-            <div class="wrapper-button"><b-button>Enviar</b-button></div>
+              <div class="inputValor">
+                <span> R$ </span>
+                <money v-bind="money" id="valor" v-model="price"></money>
+              </div>
+              <div class="btnsAdicionar">
+                <button class="adicionar">+ 100,00</button>
+                <button class="adicionar">+ 500,00</button>
+                <button class="adicionar">+ 1000,00</button>
+              </div>
+              <b-form-group id="input-group" label="Nome:" label-for="input-2">
+                <b-form-input id="input" required></b-form-input>
+              </b-form-group>
+              <b-form-group id="input-group" label="Email:" label-for="input-2">
+                <b-form-input id="input2" required></b-form-input>
+              </b-form-group>
+              <b-form-group
+                id="input-group"
+                label="Telefone:"
+                label-for="input-2"
+              >
+                <b-form-input id="input3" required></b-form-input>
+              </b-form-group>
+              <b-form-checkbox id="checkbox-1" name="checkbox-1" size="sm">
+                Sim, aceito receber emails, mensagens e ligações da empresa.
+              </b-form-checkbox>
+              <div class="wrapper-button"><b-button>Enviar</b-button></div>
+            </div>
           </div>
         </div>
       </div>
@@ -50,11 +56,33 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import { Money } from 'v-money'
+export default {
+  components: { Money },
+
+  data() {
+    return {
+      price: 123.45,
+      money: {
+        decimal: ',',
+        thousands: '.',
+        precision: 2,
+        masked: false,
+      },
+    }
+  },
+}
+</script>
 
 <style>
 * {
   font-family: 'Barlow', sans-serif;
+  line-height: normal;
+}
+.fundo {
+  background: url(@/static/images/background.svg) no-repeat fixed;
+  background-size: 100%;
 }
 .linha1 {
   min-height: 100vh;
@@ -86,12 +114,119 @@
   color: #001f0d;
 }
 .coluna2 {
-  background: lightgreen;
+  background: transparent;
 }
 .conteudo-coluna2 {
   margin-top: 15vh;
   margin-right: 20%;
   background-color: #f1f0f0;
   border-radius: 12px;
+}
+h2 {
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  color: #00823b;
+}
+span {
+  font-size: 18px;
+  font-weight: 500;
+  color: #6c757d;
+}
+.inputValor {
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+  width: 100%;
+}
+#valor {
+  color: #343a40;
+  font-weight: 500;
+  font-size: 18px;
+  border: none;
+  background-image: none;
+  background-color: transparent;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  text-align: right;
+  width: 100%;
+}
+#valor:focus-visible {
+  outline: none !important;
+}
+.btnsAdicionar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 5px;
+}
+.adicionar {
+  border: none;
+  background-color: #fff;
+  color: #6c757d;
+  font-weight: 400;
+  width: 100px;
+  border-radius: 4px;
+  margin-left: 5px;
+}
+label,
+.labelValor {
+  color: #343a40;
+  font-size: 18px;
+  font-weight: 500;
+  margin-bottom: 5px;
+}
+.form-control {
+  height: 35px;
+  padding: 5px 12px;
+  font-size: 18px;
+}
+.form-control:focus {
+  border: 1px solid #00823b;
+  box-shadow: inherit !important;
+}
+.form-group {
+  margin-bottom: 15px;
+}
+.custom-control-label {
+  font-size: 14px;
+  font-weight: 400;
+  color: #6c757d;
+}
+.custom-control.custom-checkbox.b-custom-control-sm {
+  margin-bottom: 15px;
+}
+.custom-checkbox .custom-control-input:checked ~ .custom-control-label::before {
+  background-color: #00823b;
+  box-shadow: inherit !important;
+  border: inherit !important;
+}
+.wrapper-button {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.btn.btn-secondary {
+  width: 150px;
+  height: 45px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 500;
+  background: #00823b;
+  border: none;
+  border-radius: 6px;
+  margin-bottom: 5px;
+}
+.btn.btn-secondary:active {
+  background-color: #003b1a !important;
+}
+.btn.btn-secondary:focus {
+  box-shadow: none;
+  outline: none;
+}
+.btn.btn-secondary:focus-visible {
+  outline: none;
 }
 </style>
