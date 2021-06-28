3fca700b713dcd2dbfd5f34922ad3615c68cea93
Luiza Rafaela Padilha <luizarafaela1211@gmail.com>
2021-06-28 15:33:03 -0300

    reestruturacao sicredi

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="esquerda">
+      <a href="http://www.sicredi.com.br" class="logoImg"
+        ><img
+          src="https://www.sicredi.com.br/static/assets/novo/logo-cor.svg"
+          alt="Sicredi logo"
+          class="logo"
+      /></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.header {
+  background-color: #fff;
+  height: 80px;
+  width: 100%;
+  position: fixed;
+  z-index: 1001;
+  display: flex;
+  justify-content: flex-end;
+  -webkit-box-shadow: 0 2px 10px -2px rgb(0 0 0 / 20%);
+  -moz-box-shadow: 0 2px 10px -2px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 10px -2px rgb(0 0 0 / 20%);
+}
+.esquerda {
+  height: 100%;
+  margin-right: 26px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="fundo">
+    <Header></Header>
     <div v-if="!acceptedCookies" class="cookies-container px-lg-5">
       <div
         class="
@@ -33,19 +34,10 @@
         <div class="col-sm-12 col-md-12 col-lg-6 coluna1">
           <div class="row px-3">
             <div class="conteudo-coluna1 col-12 px-0">
-              <div class="logo">
-                <img
-                  src="@/static/images/SimulaCred.svg"
-                  alt="Logo SimulaCred"
-                />
-              </div>
-              <p class="subtitulo mt-4">Crédito rápido e fácil.</p>
-              <p class="legenda mt-4 col-lg-8 col-md-8 px-0">
-                Com a nossa empresa, fazer um empréstimo é muito simples.
-                Contamos com métodos que facilitam esse processo.
-              </p>
-              <p class="chamada col-lg-8 col-md-8 px-0">
-                Simule em nossa ferramenta as condições de crédito
+              <p class="chamada col-lg-9 col-md-8 px-0">
+                Faça aqui sua
+                <span class="importante">simulação de crédito</span> consignado
+                e receba nosso contato.
               </p>
             </div>
           </div>
@@ -72,31 +64,27 @@
                   + 10.000,00
                 </button>
               </div>
-              <b-form-group id="input-group" label="Nome:" label-for="input-2">
+              <b-form-group id="input-group" label-for="input-2">
                 <b-form-input
-                  id="input"
+                  id="input1"
                   v-model="nome"
-                  placeholder="digite o seu nome"
+                  placeholder="Nome"
                   required
                 ></b-form-input>
               </b-form-group>
-              <b-form-group id="input-group" label="Email:" label-for="input-2">
+              <b-form-group id="input-group" label-for="input-2">
                 <b-form-input
                   id="input2"
                   v-model="email"
-                  placeholder="digite o seu e-mail"
+                  placeholder="E-mail"
                   required
                 ></b-form-input>
               </b-form-group>
-              <b-form-group
-                id="input-group"
-                label="Whatsapp:"
-                label-for="input-2"
-              >
+              <b-form-group id="input-group" label-for="input-2">
                 <b-form-input
                   id="input3"
                   v-model="whatsapp"
-                  placeholder="digite o seu whatsapp"
+                  placeholder="Whatsapp"
                   required
                 ></b-form-input>
               </b-form-group>
@@ -130,9 +118,10 @@
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
 import { Money } from 'v-money'
 export default {
-  components: { Money },
+  components: { Money, Header },
   data() {
     return {
       permiteContato: true,
@@ -201,14 +190,11 @@ export default {
 
 <style>
 * {
-  font-family: 'Barlow', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   line-height: normal;
 }
 @media (min-width: 768px) and (max-width: 991.98px) {
-  .fundo {
-    background: url(@/static/images/backgroundTablet.svg) no-repeat fixed !important;
-    background-size: 100% !important;
-  }
   .cookies-container {
     min-height: 20vh !important;
   }
@@ -223,10 +209,6 @@ export default {
   }
 }
 @media (max-width: 767.98px) {
-  .fundo {
-    background: url(@/static/images/backgroundMobile.svg) no-repeat fixed !important;
-    background-size: 100% !important;
-  }
   .cookies-container {
     min-height: 180px !important;
   }
@@ -253,8 +235,7 @@ export default {
   }
 }
 .fundo {
-  background: url(@/static/images/background.svg) no-repeat fixed;
-  background-size: 100%;
+  background-color: #f8f9f7;
 }
 .cookies-container {
   z-index: 1000;
@@ -298,9 +279,6 @@ export default {
   margin-top: 20vh;
   margin-right: 20%;
 }
-.logo {
-  height: 40px;
-}
 .subtitulo {
   font-size: 24px;
   font-weight: 700;
@@ -312,9 +290,13 @@ export default {
   color: #001f0d;
 }
 .chamada {
-  font-size: 18px;
-  font-weight: 700;
-  color: #001f0d;
+  font-size: 45px;
+  font-weight: 300;
+  color: #757575;
+}
+.importante {
+  color: #3fa110;
+  font-size: 45px;
 }
 .coluna2 {
   background: transparent;
@@ -322,27 +304,24 @@ export default {
 .conteudo-coluna2 {
   margin-top: 15vh;
   margin-right: 20%;
-  background-color: #f1f0f0;
-  border-radius: 12px;
-  box-shadow: rgb(191, 194, 190) 0 0 10px;
 }
 h2 {
   font-size: 36px;
-  font-weight: 500;
+  font-weight: 400;
   text-align: center;
-  color: #00823b;
+  color: #3fa110;
 }
 .labelValor {
-  color: #343a40;
+  color: #5a645b;
   font-size: 18px;
-  font-weight: 500;
+  font-weight: 300;
   text-align: center;
   margin-bottom: 15px;
 }
 span {
   font-size: 36px;
-  font-weight: 500;
-  color: #6c757d;
+  font-weight: 400;
+  color: #757575;
 }
 .inputValor {
   display: flex;
@@ -351,8 +330,8 @@ span {
   width: 100%;
 }
 #valor {
-  color: #343a40;
-  font-weight: 500;
+  color: #323c32;
+  font-weight: 400;
   font-size: 36px;
   border: none;
   background-image: none;
@@ -374,9 +353,9 @@ span {
   margin-top: 15px;
 }
 .adicionar {
-  border: none;
-  background-color: #fff;
-  color: #6c757d;
+  border: 1px solid #bbb !important;
+  background-color: transparent;
+  color: #757575;
   font-size: 18px;
   font-weight: 400;
   width: 100px;
@@ -384,34 +363,50 @@ span {
   border-radius: 4px;
   margin: 0 0 15px 10px;
 }
-label {
-  color: #343a40;
-  font-size: 18px;
-  font-weight: 500;
-  margin-bottom: 5px;
-}
 .form-control {
   height: 45px;
   padding: 5px 12px;
   font-size: 18px;
+  background-color: transparent !important;
+  border: none !important;
+  border-radius: 0;
+  border-bottom: 1px solid #bbb !important;
 }
 .form-control:focus {
-  border: 1px solid #00823b;
+  border-bottom: 2px solid #3fa110 !important;
   box-shadow: inherit !important;
 }
 .form-group {
   margin-bottom: 15px;
 }
+#input1::placeholder {
+  font-size: 16px;
+  font-weight: 400;
+  font-family: Arial, Helvetica, sans-serif !important;
+  color: #757575;
+}
+#input2::placeholder {
+  font-size: 16px;
+  font-weight: 400;
+  font-family: Arial, Helvetica, sans-serif !important;
+  color: #757575;
+}
+#input3::placeholder {
+  font-size: 16px;
+  font-weight: 400;
+  font-family: Arial, Helvetica, sans-serif !important;
+  color: #757575;
+}
 .custom-control-label {
   font-size: 14px;
-  font-weight: 400;
-  color: #6c757d;
+  font-weight: 300;
+  color: #5a645b;
 }
 .custom-control.custom-checkbox.b-custom-control-sm {
   margin-bottom: 15px;
 }
 .custom-checkbox .custom-control-input:checked ~ .custom-control-label::before {
-  background-color: #00823b;
+  background-color: #3fa110;
   box-shadow: inherit !important;
   border: inherit !important;
 }
@@ -426,8 +421,8 @@ label {
   height: 45px;
   color: #fff;
   font-size: 20px;
-  font-weight: 500;
-  background: #00823b;
+  font-weight: 400;
+  background: #3fa110;
   border: none;
   border-radius: 6px;
   margin-bottom: 5px;
@@ -445,7 +440,8 @@ label {
 footer {
   width: 100%;
   height: 80px;
-  background-color: #00823b;
+  margin-top: 8vh !important;
+  background-color: #212529;
   color: #fff;
   text-align: center;
   display: flex;
